96ce4ef2e8afacb589cf576181d8d1334f92baac
allanchen <allanchen@tencent.com>
2021-02-07 16:58:20 +0800

    log

--- ginhttpsvr/handler.go
+++ ginhttpsvr/handler.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/allanchen1214/bingo/log"
 	"github.com/gin-gonic/gin"
@@ -30,9 +32,26 @@ func RegisterOrderRouter(r *gin.Engine) {
 	h := newOrderHandler()
 	g := r.Group("/order")
 	g.GET("/list", h.List)
+	g.GET("/get/:id", h.GetByID)
 }
 
 func (h *OrderHandler) List(c *gin.Context) {
 	log.Info("list all orders", zap.Any("data", orders))
 	c.JSON(http.StatusOK, orders)
+}
+
+func (h *OrderHandler) GetByID(c *gin.Context) {
+	sid := c.Param("id")
+	id, _ := strconv.Atoi(sid)
+	id64 := uint64(id)
+	log.Info("get by id req", zap.Uint64("id", id64))
+	for _, item := range orders {
+		if item.OrderID == id64 {
+			log.Info("get by id resp", zap.Any("data", item))
+			c.JSON(http.StatusOK, item)
+			return
+		}
+	}
+	log.Info("get by id resp", zap.Error(errors.New("NotFound")))
+	c.JSON(http.StatusBadRequest, "NotFound")
 }
